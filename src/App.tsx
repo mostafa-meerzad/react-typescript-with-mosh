54ff584c6f3f1dd4b2213c402b604383f9b4a12e
mostafa <mostafameerzad@gmail.com>
2024-08-14 10:52:53 +0430

    create first React component with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import ReminderList from "./components/ReminderList";
+import Reminder from "./types/Reminder";
+
+
+const reminders: Reminder[] = [
+  {id:1, title:"learn MySQL"},
+  {id:1, title:"learn Git"},
+  {id:1, title:"learn Python"},
+  {id:1, title:"learn Javascript"},
+]
 const App = () => {
+
+
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <main>
+      <h1>Reminder App With Typescript</h1>
+      <br />
+      <ReminderList items={reminders}/>
+    </main>
+  );
+};
+export default App;
